4fd7dedf2223a7ff10460a909b0381be4117b1c5
Michaël Goossens <michael.goossens@fednot.be>
2024-12-07 21:04:50 +0100

    AoC 2024 Day 7

--- aoc2024/Day0.py
+++ aoc2024/Day0.py
@@ -1,3 +1,4 @@
+import time
 import Utils
 from aocd import submit
 
@@ -18,22 +19,34 @@ if __name__ == "__main__":
     puzzle_input = Utils.read_input(f"input/day{day}_input.txt")
 
     print("Part 1")
+    start_time = time.time()
     p1_tst_result = solve(tst_input)
     print(f"Test solution: {p1_tst_result}.")
+    elapsed_time = time.time() - start_time
+    print(f"Time taken p1 test: {elapsed_time:.2f} seconds")
     if p1_tst_result == p1_expected_tst_result:
         print("Test passed - Calculating real input now")
+        start_time = time.time()
         p1_result = solve(puzzle_input)
+        elapsed_time = time.time() - start_time
+        print(f"Time taken p1: {elapsed_time:.2f} seconds")
         submit(p1_result, part="a", day=day, year=year)
     else:
         print("Test failed")
 
     print()
     print("Part 2")
+    start_time = time.time()
     p2_tst_result = solve(tst_input)
+    elapsed_time = time.time() - start_time
+    print(f"Time taken p2 test: {elapsed_time:.2f} seconds")
     print(f"Test solution: {p2_tst_result}.")
     if p2_tst_result == p2_expected_tst_result:
         print("Test passed - Calculating real input now")
+        start_time = time.time()
         p2_result = solve(puzzle_input)
+        elapsed_time = time.time() - start_time
+        print(f"Time taken p2: {elapsed_time:.2f} seconds")
         submit(p2_result, part="b", day=day, year=year)
     else:
         print("Test failed")
